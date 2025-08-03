20abbe2a43da84cb11e2c19926cd510f04968670
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-03 16:13:48 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
       color: '#FFFACD',
       brushImage: 'https://cdn.prod.website-files.com/65576d30478026e86cc17b29/655cfb60bbb70cd3cb60d696_paint-roller-%20greenpng.png',
       productCode: 'N°2570',
-      brushColor: '#FFFACD' // لون الفرشاة مطابق للدائرة
+      brushColor: 'red' // لون الفرشاة مطابق للدائرة
     },
     {
       image: 'https://images.pexels.com/photos/1080721/pexels-photo-1080721.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&fit=crop',
