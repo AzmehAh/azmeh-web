4aeff6ffe22e361fd14b6ecc2996f806807ef6cc
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-04 11:09:52 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -90,10 +90,7 @@ const Hero = () => {
               {isActive && (
                 <motion.div
                   className="absolute top-1/2 right-8 transform -translate-y-1/2 w-[80%] text-white z-10 flex flex-col justify-center"
-                  initial={{ opacity: 0, x: 50 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  exit={{ opacity: 0, x: 50 }}
-                  transition={{ duration: 0.1 }}
+                 
                   style={{ textShadow: "0 0 8px rgba(0,0,0,0.8)" }}
                  >
                   <h2 className="text-3xl font-extrabold mb-2">{category.title}</h2>
