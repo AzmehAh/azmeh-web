1c8fd6927cdd3e12a051c7af2f406465dc799107
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-04 14:10:04 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -66,10 +66,7 @@ const Hero = () => {
     flex: isActive ? 5 : 1,
     transform: isActive ? 'rotate(0deg)' : 'rotate(-5deg)',
     marginLeft: isActive ? '0' : '-25px',
-    marginRight: isActive 
-      ? (index === paintCategories.length -1 ? '0' : '-25px') 
-      : '-25px',
-  }}
+    
   style={{
     transformOrigin: 'center center',
   transform: 'none' // إضافة هذه السطر لضبط التحويل الابتدائي
