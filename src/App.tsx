7e7be8d66b70f3de8e44af5e3dce28e8f76f3748
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-19 16:03:39 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
-
+import ColorInspiration from './components/ColorInspiration';
 import InspirationGallery from './components/InspirationGallery';
 import FAQ from './components/FAQ';
 import Brands from './components/Brands';
@@ -12,10 +12,10 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen">
-      <Header />
+      <Header /> 
       <Hero />
       <Services />
-   
+      <ColorInspiration />
       <InspirationGallery />
       <FAQ />
       <Brands />
