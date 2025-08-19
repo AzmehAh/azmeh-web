f0a636c748728a1f357f1b1f4951792f073b798d
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-19 16:00:36 +0300

    Updated InspirationGallery.tsx

--- src/components/InspirationGallery.tsx
+++ src/components/InspirationGallery.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 
 const InspirationGallery = () => {
   const [hoveredImage, setHoveredImage] = useState<number | null>(null);
