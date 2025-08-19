363e6e62a1cdab35dc67605bb97b778adf80a8a1
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-19 16:01:59 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
-import ColorInspiration from './components/ColorInspiration';
+
 import InspirationGallery from './components/InspirationGallery';
 import FAQ from './components/FAQ';
 import Brands from './components/Brands';
@@ -15,7 +15,7 @@ function App() {
       <Header />
       <Hero />
       <Services />
-      <ColorInspiration />
+   
       <InspirationGallery />
       <FAQ />
       <Brands />
