0353bf40cd8d8614b3f925f8b74eb597a5e90362
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-31 13:53:08 +0300

    Updated ProductDetail.tsx

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -180,8 +180,10 @@ const ProductDetail = () => {
                         ))}
                       </div>
                     )}
-              
-         
+                  </div>
+                </div>
+              </div>
+            </div>
           </section> 
       {/* Product Specifications */}
       <section className="py-16 bg-white">
