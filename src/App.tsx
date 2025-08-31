81c7b4d76252cffa10c711aad7bcecafb3f56183
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-08-31 13:28:09 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Products from './components/Products';
 import ProductDetail from './components/ProductDetail';
 import Systems from './components/Systems';
 import BulletinDetail from './components/BulletinDetail';
-import FAQ from './components/FAQ';
+import FAQ from './components/FAQ2';
 import Troubleshooting from './components/Troubleshooting';
 import Brands from './components/Brands';
 import SocialMedia from './components/SocialMedia';
