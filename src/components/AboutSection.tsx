6b17932a3f099139fe82d206571ed0982032bb0a
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-01 14:11:04 +0300

    Updated AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -129,16 +129,16 @@ const AboutSection = () => {
               ))}
             </div>
  
-            {/* Read More Button */}
-            <motion.button
-              onClick={() => navigate('/about')}
-              whileHover={{ scale: 1.02 }}
-              whileTap={{ scale: 0.98 }}
-              className="group inline-flex items-center space-x-3 px-8 py-3 border-2 border-gray-300 text-gray-700 font-semibold rounded-lg hover:border-[#2C5DB6] hover:text-[#2C5DB6] transition-all duration-300"
-            >
-              <span>READ MORE</span>
-              <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform" />
-            </motion.button>
+           <motion.button
+  onClick={() => navigate('/about')}
+  whileHover={{ scale: 1.02 }}
+  whileTap={{ scale: 0.98 }}
+  className="inline-flex items-center space-x-3 px-6 py-2 border-2 border-gray-300 text-gray-700 font-semibold rounded-lg hover:border-[#2C5DB6] hover:text-[#2C5DB6] transition-all duration-300 w-auto"
+>
+  <span>READ MORE</span>
+  <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform" />
+</motion.button>
+
           </motion.div>
         </div>
       </div>
