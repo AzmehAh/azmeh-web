8c567dd6942792004910b6012f6ba3a079f91011
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-01 16:40:23 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -190,12 +190,12 @@ const Hero = () => {
               {/* المحتوى النصي */}
               {isActive && (
                 <motion.div
-                  className="absolute p-10 inset-0 flex flex-col justify-center items-center mt-40 p-12 z-20"
+                  className="absolute inset-0 flex flex-col justify-center items-center mt-40 p-12 z-20"
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ delay: 0.3, duration: 0.5 }}
                 >
-                  <p className="text-xl text-white mb-6 max-w-lg text-center drop-shadow-lg bg-black bg-opacity-40 p-4 rounded-lg">
+                  <p className="text-xl text-white mb-6 max-w-lg text-center drop-shadow-lg  bg-opacity-40 p-4 rounded-lg">
                     {category.description}
                   </p>
                   <button
