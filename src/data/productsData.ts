75bf1393b575bffd9a37a87279863a611c856ee8
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-01 10:36:08 +0300

    Updated productsData.ts

--- src/data/productsData.ts
+++ src/data/productsData.ts
@@ -692,7 +692,7 @@ export const productsData: Product[] = [
   }
 ],
 {
-  id: "11",
+  id: "Paint product",
   name: "EPX02 Epoxy Polyamine",
   category: "Epoxy",
   description: "أساس إيبوكسي بوليامين ذو متانة عالية ومتعدد الاستخدامات مكوّن من مركبين.",
