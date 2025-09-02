546720f99a00ad0e91d4e63237fb2e37600a0b8e
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-02 11:34:39 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -135,7 +135,7 @@ const Hero = () => {
               initial={{ flex: 1 }}
               animate={{
                 flex: isActive ? 5 : 1,
-                transform: isActive ? "rotate(0deg)" : "rotate(-6deg)",
+                transform: isActive ? "rotate(0deg)" : "rotate(-5deg)",
                 marginLeft: "-25px",
                 marginRight: "-25px",
               }}
