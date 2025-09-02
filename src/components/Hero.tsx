d2370d11834f62fe957c3110323240a96d5bff94
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-02 11:39:59 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -122,7 +122,7 @@ const Hero = () => {
 
   return (
     <div className="relative w-full h-screen overflow-hidden ">
-      <div className="flex h-full flex-row-reverse">
+      <div className="flex h-full "> 
         {paintCategories.map((category, index) => {
           const isActive = activeIndex === index;
 
