85f381cc055602abab59c5bd1c4d450f83452e09
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-02 11:49:20 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -80,7 +80,7 @@ const AnimatedTitle = ({ text, isActive }) => {
         display: "flex",
         perspective: "1000px",
         transformStyle: "preserve-3d",
-        fontSize: isActive ? "4.5rem" : "5.8rem",
+        fontSize: isActive ? "4.5rem" : "5rem",
         fontWeight: "900",
         fontStyle: "italic",
         color: "white",
