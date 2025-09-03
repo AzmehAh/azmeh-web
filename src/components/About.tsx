c3be46f9381f659f9991f08163fb4049dc12b5b0
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-03 15:29:56 +0300

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -424,7 +424,7 @@ const About = () => {
           <div
             className="w-16 h-16 rounded-2xl flex items-center justify-center transition-transform duration-300"
           >
-            <goal.icon className="w-8 h-8" style={{ color: goal.color }} />
+            <goal.icon className="w-8 h-8 group-hover:text-[#0055A3]" />
           </div>
           <h3 className="text-2xl font-bold text-gray-900">{goal.title}</h3>
         </div>
