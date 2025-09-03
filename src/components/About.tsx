828bfcc2a3d2a89ad9f7c9e1a6c9f763efec96d7
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-03 13:44:38 +0300

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -423,9 +423,9 @@ const About = () => {
         {/* أيقونة + عنوان أفقي */}
         <div className="flex items-center gap-4 mb-4">
           <div
-            className={`w-16 h-16 rounded-2xl text-gradient-to-br ${goal.gradient} flex items-center justify-center group-hover:scale-110 transition-transform duration-300`}
+            className={`w-16 h-16 rounded-2xl flex items-center justify-center group-hover:scale-110 transition-transform duration-300`}
           >
-            <goal.icon className="w-8 h-8 text-white" />
+            <goal.icon className="w-8 h-8  text-gradient-to-br ${goal.gradient} " />
           </div>
           <h3 className="text-2xl font-bold text-gray-900">{goal.title}</h3>
         </div>
