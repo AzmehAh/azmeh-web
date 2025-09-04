a0c3e19adf5f3731424c1c2bd2f351a3b0484dd6
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-04 15:17:11 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -190,7 +190,7 @@ const Hero = () => {
   <motion.div
     className="absolute z-20"
     style={{
-      top: "60px",   // ثابت، ما بيتغير مع حجم الكارد
+      top: "40px",   // ثابت، ما بيتغير مع حجم الكارد
       left: "50px",     // ثابت، يبقى دائماً نفس البداية
       maxWidth: "480px"
     }}
