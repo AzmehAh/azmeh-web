a6d5fd5384f1db1f05daeb86e16446af6664c746
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-04 14:21:06 +0300

    Updated AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -35,7 +35,7 @@ const AboutSection = () => {
   return (
     <section className="py-24 bg-gray-50 relative overflow-hidden">
       {/* Background decorative elements */}
-      <div className="absolute top-0 left-0 w-96 h-96 bg-[#0055A3]/10 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute top-0 left-0 w-96 h-98 bg-[#0055A3]/10 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"></div>
       <div className="absolute bottom-0 right-0 w-80 h-80 bg-blue-300/20 rounded-full blur-2xl translate-x-1/2 translate-y-1/2"></div>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
