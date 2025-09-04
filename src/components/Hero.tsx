bfd1816aea7962305aeb1670f6756acb25cceb93
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-04 15:13:13 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -198,7 +198,7 @@ const Hero = () => {
     animate={{ opacity: 1, y: 0 }}
     transition={{ delay: 0.3, duration: 0.5 }}
   >
-    <p className="text-xl mb-6 text-white leading-relaxed drop-shadow-lg  p-4 rounded-lg">
+    <p className="text-xl mb-3 text-white leading-relaxed drop-shadow-lg  p-4 rounded-lg">
       {category.description}
     </p>
     <motion.button
