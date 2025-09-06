861a0d4a0eb5416dbf49906d786607d63d82cd6d
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-06 17:23:44 +0300

    Updated AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -75,13 +75,11 @@ const AboutSection = () => {
 
                 {/* Logo on top */}
                 <div className="w-16 h-16 sm:w-20 sm:h-20 mb-4">
-                 <img 
-      src="/images/Azmeh-Paints-Logo.png" 
-      alt="AL AZMEH PAINTS" 
-      className={`h-10 w-auto transition-all duration-300 ${
-        isScrolled ? "filter-none" : "brightness-0 invert"
-      }`}
-    />
+                  <img 
+                    src="/images/Azmeh-Paints-Logo.png" 
+                    alt="Al Azmeh Paints" 
+                    className="w-full h-full brightness-0 invert object-contain"
+                  />
                 </div>
                 
                 {/* Counter */}
