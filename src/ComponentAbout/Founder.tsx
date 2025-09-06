a58bf7d4048a6a2310a93f485674de3d943495d5
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-06 16:39:09 +0300

    Updated Founder.tsx

--- src/ComponentAbout/Founder.tsx
+++ src/ComponentAbout/Founder.tsx
@@ -61,11 +61,11 @@ const Founder = () => {
 
             <div className="mt-8 grid grid-cols-2 gap-6">
               <div className="text-center">
-                <div className="text-3xl font-bold text-[0055A3] mb-2">80</div>
+                <div className="text-3xl font-bold text-[#0055A3] mb-2">80</div>
                 <div className="text-gray-400">Years of Leadership</div>
               </div>
               <div className="text-center">
-                <div className="text-3xl font-bold text-green-400 mb-2">1955</div>
+                <div className="text-3xl font-bold text-[#0055A3]  mb-2">1955</div>
                 <div className="text-gray-600">Company Founded</div>
               </div>
             </div>
