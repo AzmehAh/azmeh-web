ca65e105cd5b974039890972f3834bd8dc5ce621
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-07 11:12:16 +0300

    Updated Goals.tsx

--- src/ComponentAbout/Goals.tsx
+++ src/ComponentAbout/Goals.tsx
@@ -43,7 +43,7 @@ const Goals = () => {
             return (
               <div
                 key={index}
-                className="group bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl  transition-all duration-100"
+                className="group bg-white rounded-2xl shadow-lg overflow-hidden  transition-all duration-100"
               >
                 {/* الخط العلوي الأزرق */}
                 <div className="relative h-2 overflow-hidden">
