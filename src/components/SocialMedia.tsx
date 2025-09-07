a8e4b9435bbe5a92e9d6008013e5cc50c21afaab
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-07 13:39:18 +0300

    Updated SocialMedia.tsx

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -64,9 +64,9 @@ const SocialMedia = () => {
    <a
   href="#"
   className="flex items-center bg-[#0055a3] text-white rounded-full px-4 py-3 h-12 overflow-hidden transition-all duration-500 ease-in-out w-12 hover:w-60 group shadow-lg hover:shadow-2xl origin-left whitespace-nowrap"
->
+> 
   <div className="flex items-center  justify-start w-full">
-    <Instagram className="w-5 h-5  " />
+    <Instagram className="w-5 h-5 flex-shrink-0 " />
     <span className="ml-3 opacity-0 group-hover:opacity-100 transition-all duration-300">
       Follow on Instagram 
     </span>
