66e049936037cab545a147af1b4e2465bbb1672b
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-07 13:53:14 +0300

    Updated SocialMedia.tsx

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -30,7 +30,7 @@ const SocialMedia = () => {
           <h3 className="text-sm uppercase text-[#0055A3] mb-2 tracking-widest">
       Creative Vibes
     </h3>
-          <h2 className="text-4xl font-extrabold text-gray-900 mb-4">
+          <h2 className="text-4xl font-bold text-gray-900 mb-4">
             Follow us for inspiration
           </h2>
           
