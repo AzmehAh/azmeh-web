f8234a7c0b184c0cd840663c4d09095356b4f9fc
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-07 11:22:24 +0300

    Updated CTA.tsx

--- src/ComponentAbout/CTA.tsx
+++ src/ComponentAbout/CTA.tsx
@@ -22,14 +22,14 @@ const CTA = () => {
             <motion.button
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              className="bg-white text-[#2C5DB6] px-8 py-4  font-bold text-lg hover:shadow-2xl transition-all duration-300"
+              className="bg-white text-[#2C5DB6] px-8 py-4 rounded-lg font-bold text-lg hover:shadow-2xl transition-all duration-300"
             >
               Get In Touch
             </motion.button>
             <motion.button
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              className="bg-white/20 backdrop-blur-sm text-white px-8 py-4  font-bold text-lg hover:bg-white/30 transition-all duration-300"
+              className="bg-white/20 backdrop-blur-sm rounded-lg text-white px-8 py-4  font-bold text-lg hover:bg-white/30 transition-all duration-300"
             >
               View Our Products
             </motion.button>
