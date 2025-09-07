deb7f1f8fdfb4404ed240115dacf306e7777fcd4
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-07 14:15:06 +0300

    Updated SocialMedia.tsx

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -27,7 +27,7 @@ const SocialMedia = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         {/* العنوان والنص */}
-        <div className="text-center mb-16">
+        <div className="text-center mb-6">
              <motion.h3
     initial={{ opacity: 0, y: 20 }}
     whileInView={{ opacity: 1, y: 0 }}
