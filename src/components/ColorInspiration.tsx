00b91b6c5a504d3a135ec9cc073701ce3e3af1ae
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-08 17:16:00 +0300

    Updated ColorInspiration.tsx

--- src/components/ColorInspiration.tsx
+++ src/components/ColorInspiration.tsx
@@ -59,7 +59,8 @@ const ColorInspiration = () => {
 
 
         {/* Grid */}
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-16 lg:gap-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-20
+">
           {colorSwatches.map((swatch, index) => (
             <div
               key={index}
