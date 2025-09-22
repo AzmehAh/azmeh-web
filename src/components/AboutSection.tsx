b23e61d3f90d61116d928272c6d702c8cf437d93
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-22 16:01:25 +0300

    Updated AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -30,7 +30,7 @@ const AboutSection = () => {
   }, [targetYears]);
 
   return (
-    <section className="py-20 bg-white">
+    <section className="py-20 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-center">
 
