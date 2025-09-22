8edf078f661f379d70fe97a117e374b2bb30d567
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-22 16:13:47 +0300

    Updated ColorInspiration.tsx

--- src/components/ColorInspiration.tsx
+++ src/components/ColorInspiration.tsx
@@ -87,7 +87,7 @@ const ColorInspiration = () => {
             {featuredProducts.map((product, index) => (
   <div
   key={product.id}
-  className="relative group cursor-pointer w-52 h-[280px] mx-auto overflow-hidden"
+  className="relative group cursor-pointer w-52 h-[280px] mx-auto "
   onMouseEnter={() => setHoveredColor(index)}
   onMouseLeave={() => setHoveredColor(null)}
 >
