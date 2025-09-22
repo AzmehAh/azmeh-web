2d315884a75bff396efa4c7de49caf50c75590b4
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-22 16:02:00 +0300

    Updated ColorInspiration.tsx

--- src/components/ColorInspiration.tsx
+++ src/components/ColorInspiration.tsx
@@ -54,7 +54,7 @@ const ColorInspiration = () => {
   };
 
   return (
-    <section className="py-18 bg-white">
+    <section className="py-20 bg-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-10 lg:px-14">
         {/* Title */}
         <div className="text-center mb-10">
