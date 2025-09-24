2a0f2c0ca7e61803330ce8856ebaf415e45ea92f
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-24 17:13:08 +0300

    Updated About.tsx

--- src/ComponentAbout/About.tsx
+++ src/ComponentAbout/About.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import AboutSection from './AboutSection';
 import Values from './Values';
 import Goals from './Goals';
-import Founder from './Founder';
 import CTA from './CTA';
 import AboutFounderSection from './AboutFounderSection';
 
@@ -13,7 +12,7 @@ const About = () => {
       <AboutFounderSection />
       <Values /> 
       <Goals />
-      <Founder />
+   
       <CTA />
     </div>
   );
