29d7e751ee72e6bfe6124f6f09a2ada7e274326e
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-25 16:11:20 +0300

    Updated ColorInspiration.tsx

--- src/components/ColorInspiration.tsx
+++ src/components/ColorInspiration.tsx
@@ -112,9 +112,11 @@ const ColorInspiration = () => {
   />  
 
   {/* العنوان */}
-  <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-center text-gray-800 ">
+  {hoveredColor !== index && (
+  <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-center text-gray-800 z-20">
     <span className="block text-lg font-semibold">{product.name}</span>
   </div>
+)}
 </div> 
      
             ))}
