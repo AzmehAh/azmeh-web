d29ec0765e712bfadd19f434a8597dec200d2fc4
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-25 14:05:18 +0300

    Updated BulletinForm.tsx

--- src/components/admin/BulletinForm.tsx
+++ src/components/admin/BulletinForm.tsx
@@ -598,7 +598,7 @@ let imgTag = `<img src="${imageUrl}" ${styleAttr ? `style="${styleAttr}"` : ''} 
     </div>
   )}
 </div>
-          
+        </div>  
             {/* Content */}
             <div className="mt-6">
               <label className="block text-sm font-medium text-gray-700 mb-2">
