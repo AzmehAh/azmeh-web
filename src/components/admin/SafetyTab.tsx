8a7383a6077afb33a783af42835a1841c959acfd
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-30 13:55:50 +0300

    Updated SafetyTab.tsx

---
+++ src/components/admin/SafetyTab.tsx
@@ -0,0 +1,46 @@
+// src/components/admin/SafetyTab.tsx
+import React from 'react';
+import { ArrayInputField } from './FormComponents';
+
+interface Props {
+  data: any;
+  onChange: (field: string, value: any) => void;
+}
+
+export const SafetyTab: React.FC<Props> = ({ data, onChange }) => {
+  const handleArrayChange = (field: string, index: number, value: string) => {
+    const arr = [...(data[field] || [])];
+    arr[index] = value;
+    onChange(field, arr);
+  };
+
+  const addArrayItem = (field: string) => {
+    const arr = [...(data[field] || []), ''];
+    onChange(field, arr);
+  };
+
+  const removeArrayItem = (field: string, index: number) => {
+    const arr = [...(data[field] || [])];
+    arr.splice(index, 1);
+    onChange(field, arr);
+  };
+
+  return (
+    <div className="space-y-6">
+      <ArrayInputField
+        label="Safety Precautions"
+        items={data.safety_precautions || []}
+        onAdd={() => addArrayItem('safety_precautions')}
+        onRemove={(idx) => removeArrayItem('safety_precautions', idx)}
+        onChange={(idx, val) => handleArrayChange('safety_precautions', idx, val)}
+      />
+      <ArrayInputField
+        label="First Aid Measures"
+        items={data.safety_first_aid || []}
+        onAdd={() => addArrayItem('safety_first_aid')}
+        onRemove={(idx) => removeArrayItem('safety_first_aid', idx)}
+        onChange={(idx, val) => handleArrayChange('safety_first_aid', idx, val)}
+      />
+    </div>
+  );
+}; 
