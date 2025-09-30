ebe8fac02d481fb281a3294da269717f98515a94
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-30 17:17:40 +0300

    Updated ApplicationTab.tsx

--- src/components/admin/ApplicationTab.tsx
+++ src/components/admin/ApplicationTab.tsx
@@ -52,7 +52,7 @@ export const ApplicationTab: React.FC<Props> = ({ data, onChange }) => {
       <InputField 
         label="Thinner / Cleaner" 
         value={data.thinner_cleaner || ''} 
-        onChange={(v) => handleChange('thinner', v)} 
+        onChange={(v) => handleChange('Thinner / Cleaner', v)} 
       />
       <InputField 
         label="Application Temperature" 
