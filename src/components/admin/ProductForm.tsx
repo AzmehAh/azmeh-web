8e9876afcef3f75b5b2655dd97a3bc2bf5db0a39
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-09-30 11:29:56 +0300

    Updated ProductForm.tsx

--- src/components/admin/ProductForm.tsx
+++ src/components/admin/ProductForm.tsx
@@ -12,6 +12,10 @@ import { supabase } from '../../lib/supabase';
 import { useNavigate, useParams } from 'react-router-dom';
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css"; 
+import { GeneralInfoModal } from './GeneralInfoModal';
+import { ApplicationModal } from './ApplicationModal';
+import { TechnicalModal } from './TechnicalModal';
+import { DryingTimeModal } from './DryingTimeModal';
 
 // =============== أنواع البيانات المحدثة ===============
 interface ProductImage {
