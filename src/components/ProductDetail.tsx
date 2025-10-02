4beee450a78b3ee0a2f8e70ad99f32a4010b2c3e
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-02 12:45:42 +0300

    Updated ProductDetail.tsx

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -599,10 +599,9 @@ const ProductDetail = () => {
       {product.application && (
         <section className="py-16  bg-white">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-            <h2 className="text-3xl font-bold text-center text-gray-800 mb-10">
-               
-              <ClipboardList className="w-5 h-5 mr-2" />
-             Application Instruction
+            <h2 className=" text-3xl font-bold text-center text-gray-800 mb-10 flex items-center justify-center">
+              <ClipboardList className="w-8 h-8 text-green-600 mr-3" />
+              Storing Conditions
             </h2>
           
             <div className="max-w-6xl mx-auto relative">
