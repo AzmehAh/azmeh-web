86e4909aa442a73ab29f4ab1283a4bfcd0157583
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-05 17:07:25 +0300

    Updated Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -16,7 +16,7 @@ const Services = () => {
   return (
     <section className="py-8 sm:py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 sm:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-16 sm:gap-8">
           {services.map((service, index) => (
             <div
               key={index}
