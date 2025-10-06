238df526fe6935e540b5e000d107d39f434d143d
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-06 17:18:17 +0300

    Updated BulletinDetail.tsx

--- src/components/BulletinDetail.tsx
+++ src/components/BulletinDetail.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { ArrowLeft, FileText } from 'lucide-react';
+import { ArrowLeft, FileText ,ArrowRight } from 'lucide-react';
 import DOMPurify from 'dompurify';
 import { Bulletin } from '../lib/supabase';
 import { api } from '../lib/supabase';
