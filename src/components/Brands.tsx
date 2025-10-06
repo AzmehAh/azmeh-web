1c4bdf6818b15edb8d4d521e80e1e6db75d5d022
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-06 11:08:35 +0300

    Updated Brands.tsx

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 
 
 const BrandsSection = () => {
-  const { t, i18n } = useTranslation(''); // استخدم namespace 'brands'
+  const { t, i18n } = useTranslation(''); 
   const currentLang = i18n.language;
   const isRTL = currentLang === 'ar';
 
