f44858addbef3557b65a614362446ff757ea0e77
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-07 14:08:45 +0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
           <div>
             <h4 className="font-semibold mb-6 text-white text-lg">{t('footer.quickLinks')}</h4>
             <ul className={`space-y-3 ${isRTL ? 'pr-2 text-right' : 'pl-2 text-left'}`}>
-              <li><Link to="/" className="hover:text-gray-200 text-sm">{t('common.home')}</Link></li>
+              <li><Link to="/" className={`hover:text-gray-200 text-sm  ${isRTL ? 'text-right' : 'text-left'}`}>{t('common.home')}</Link></li>
               <li><Link to="/products" className="hover:text-gray-200 text-sm">{t('common.products')}</Link></li>
               <li><Link to="/blog" className="hover:text-gray-200 text-sm">{t('common.blog')}</Link></li>
               <li><Link to="/about" className="hover:text-gray-200 text-sm">{t('common.about')}</Link></li>
