61979216497cb11ac502ae2f4a591aafa259a4fc
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-08 13:31:14 +0300

    Updated ProductDetail.tsx

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -435,9 +435,9 @@ const ProductDetail = () => {
 
               <button
                 onClick={handleDownloadDatasheet}
-                className="bg-white/20  text-white px-8 py-4 rounded-xl font-bold shadow-lg hover:shadow-xl transform  transition-all duration-200 flex items-center"
+                className="bg-white/20  text-white px-8 py-4 rounded-xl font-bold shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-200 flex items-center"
               >
-                <Download className="w-6 h-6 mr-3" />
+                <Download className="w-6 h-6 mr-3" /> 
                 {t('products.download_datasheet')}
               </button>
             </div>
