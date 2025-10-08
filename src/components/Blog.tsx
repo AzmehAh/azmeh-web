513acd505ea4ce44db7ba6a4002326574af4b9b4
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-08 15:35:58 +0300

    Updated Blog.tsx

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -135,14 +135,12 @@ const Blog = () => {
   };
 
   return (
-    <div className={`min-h-screen bg-gray-50 pt-20 ${isRTL ? 'rtl' : 'ltr'}`}>
+    <div className={`min-h-screen bg-gray-50 pt-28 ${isRTL ? 'rtl' : 'ltr'}`}>
       {/* Hero Section */}
           <div className=" text-logo pt-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-      
             <h1 className="text-5xl md:text-6xl font-bold mb-6">
               {t('blog.heroTitle')}
             </h1>
-            
       </div>
 
       {/* Content */}
