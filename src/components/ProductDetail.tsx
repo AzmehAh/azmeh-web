1d53e6682070194e995d2fc48c73939a454754d9
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-08 11:55:02 +0300

    Updated ProductDetail.tsx

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -490,7 +490,7 @@ const ProductDetail = () => {
     <div className="w-full h-80 lg:h-96 bg-gray-200 rounded-2xl flex items-center justify-center relative">
       {/* Brand Logo in placeholder too (optional) */}
       {brandLogo && (
-        <div className="absolute top-0 right-4 flex items-center justify-center">
+        <div className="absolute top-0 left-39 flex items-center justify-center">
           <div className="bg-white rounded-full p-1 shadow-md">
             <img
               src={brandLogo}
