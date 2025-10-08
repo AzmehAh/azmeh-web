b30bfb20a757bd15265e136fc0b1f641ba7502dc
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-08 14:37:47 +0300

    Updated Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -388,7 +388,7 @@ const Products = () => {
             {/* Toolbar */}
             <div className="flex items-center justify-between mb-8">
               <div className="flex items-center space-x-4">
-                <h2 className="text-2xl font-bold text-gray-900">
+                <h2 className="text-2xl font-bold text-logo">
                   {t('products.ourProducts')} ({filteredProducts.length})
                 </h2>
               </div>
