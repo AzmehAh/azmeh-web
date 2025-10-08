84392449bd1c8688b8ef7b92b4c1c3ef92231e96
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-08 14:11:14 +0300

    Updated Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -255,7 +255,7 @@ const Products = () => {
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-logo"></div>
         </div>
       ) : (
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
         <div className="flex flex-col lg:flex-row gap-8"> 
           {/* Filters Sidebar */}
           <div className="lg:w-80 flex-shrink-0">
