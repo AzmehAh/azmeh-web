3796c800e557b577865dd2fd349d35f44a3d8b2b
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-08 15:17:00 +0300

    Updated JobApplication.tsx

--- src/components/JobApplication.tsx
+++ src/components/JobApplication.tsx
@@ -174,7 +174,7 @@ const JobApplication = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 pt-20">
+    <div className="min-h-screen bg-gray-50 pt-28">
       {/* Hero Section */}
      <div className=" text-logo pt-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
        
