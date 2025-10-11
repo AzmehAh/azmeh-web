25b0aa652f762032dc591833e1c392387f94cfb4
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-11 15:10:28 +0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -88,7 +88,7 @@ const Footer = () => {
               <div className="flex items-start gap-3">
                 <Mail className="w-5 h-5 text-white mt-0.5 flex-shrink-0" />
                 <span className="text-gray-300 text-sm break-all">
-                  manager@dkl-syria.com
+                azmeh@azmeh.com
                 </span>
               </div>
               {/* Phone */}
