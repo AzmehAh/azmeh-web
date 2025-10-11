a31063d9b24b8a0a5d4cd337a51cca39e32b422b
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-11 11:30:29 +0300

    Updated Goals.tsx

--- src/ComponentAbout/Goals.tsx
+++ src/ComponentAbout/Goals.tsx
@@ -28,7 +28,7 @@ const Goals = () => {
 
         {/* كتل Vision و Mission */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="bg-gray-100 rounded-2xl p-8 text-white flex flex-col gap-4">
+          <div className="bg-gray-100 shadow-lg hover:shadow-2xl rounded-2xl p-8 text-white flex flex-col gap-4">
             <div className="flex items-center gap-3">
               <Eye className="w-6 h-6 text-logo" />
               <h3 className="text-2xl font-bold text-logo">{t('goals.vision.title')}</h3>
