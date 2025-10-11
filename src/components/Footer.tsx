1cd5baf30e62ca67270532c9b94767ce52ea4387
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-11 15:11:12 +0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -95,7 +95,7 @@ const Footer = () => {
               <div className="flex items-start gap-3">
                 <Phone className="w-5 h-5 text-white mt-0.5 flex-shrink-0" />
                 <span className="text-gray-300 text-sm" dir="ltr">
-                  (+963) 988 691 712
+                  {isRTL ? "58 50 542 11 963+" : "+963 11 542 50 58"}
                 </span>
               </div>
               {/* Address */}
