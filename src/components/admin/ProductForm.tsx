92f9093a3434997e7bb167d2032792d0856ae3f7
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-19 13:04:28 +0300

    Updated ProductForm.tsx

--- src/components/admin/ProductForm.tsx
+++ src/components/admin/ProductForm.tsx
@@ -325,7 +325,7 @@ const ProductForm = () => {
         'method_of_application', 'method_of_application_ar', 'mixing_ratio', 'mixing_ratio_ar',
         'mixing_note', 'mixing_note_ar', 'mixing_steps', 'mixing_steps_ar', 'pot_life', 'pot_life_ar',
         'cleaner', 'cleaner_ar', 'thinner', 'thinner_ar', 'application_temperature', 'application_temperature_ar',
-        'curing_note', 'curing_note_ar','specific_gravity', ' specific_gravity_ar', 'note_application', 'note_application_ar',
+        'curing_note', 'curing_note_ar','specific_gravity', 'specific_gravity_ar', 'note_application', 'note_application_ar',
         'number_of_coats', 'number_of_coats_ar', 'note', 'note_ar', 'tensile_adhesion_strength', 'tensile_adhesion_strength_ar',
         'material_consumption', 'material_consumption_ar', 'viscosity', 'viscosity_ar',
         'weather_resistance', 'weather_resistance_ar', 'compressive_strength', 'compressive_strength_ar',
