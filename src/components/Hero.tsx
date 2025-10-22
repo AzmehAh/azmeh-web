12a1d0dd82dfafa083469ec4eaf9f7f0369073d2
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-22 15:16:50 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -271,7 +271,7 @@ const Hero = () => {
                       ? "auto"
                       : isRTL
                         ? "60%" 
-                        : "30%",
+                        : "20%",
                     transform: isActive
                       ? "none"
                       : "translate(-50%, -50%) rotate(-90deg)",
