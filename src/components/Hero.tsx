17811193215fb6d865d7fbe30f28347c02fff6cd
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-10-22 12:03:52 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -270,7 +270,7 @@ const Hero = () => {
                     left: isActive
                       ? "auto"
                       : isRTL
-                        ? "55%"
+                        ? "40%"
                         : "30%",
                     transform: isActive
                       ? "none"
