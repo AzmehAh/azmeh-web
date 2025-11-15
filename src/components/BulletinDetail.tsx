2d27df5c6e6a016534f889a67520ed97366210b4
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-15 16:20:54 +0300

    Updated BulletinDetail.tsx

--- src/components/BulletinDetail.tsx
+++ src/components/BulletinDetail.tsx
@@ -170,7 +170,7 @@ const BulletinDetail = () => {
             <span className="text-gray-900">{categoryDisplayName}</span>
             <span className="mx-2">/</span>
            
-            <span className="mx-2">/</span>
+           
             <span className="text-gray-900">{title}</span> 
           </div> 
         </div> 
