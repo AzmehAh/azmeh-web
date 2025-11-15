585e080a5c4f527cd9cf8b135984c4fac0118ef2
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-15 13:55:15 +0300

    Updated CTA.tsx

--- src/ComponentAbout/CTA.tsx
+++ src/ComponentAbout/CTA.tsx
@@ -9,7 +9,7 @@ const CTA = () => {
 
   return (
     <section className="py-20 bg-gray-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
