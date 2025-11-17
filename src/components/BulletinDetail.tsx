3c7fe872c7c0bcea03cf8194c3f017afc407cf92
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-17 10:39:01 +0300

    Updated BulletinDetail.tsx

--- src/components/BulletinDetail.tsx
+++ src/components/BulletinDetail.tsx
@@ -187,10 +187,10 @@ const BulletinDetail = () => {
           </button>
 
           <div className="flex items-center gap-3 mb-4">
-            <span className="px-3 py-3 bg-white/20 rounded-xl text-sm font-medium ">
+            <span className="px-4 py-3 bg-white/20 rounded-full text-sm font-medium ">
               {categoryDisplayName}
             </span>
-            <span className="px-3 py-1 bg-white/20 rounded-full text-sm font-medium">
+            <span className="px-4 py-3 bg-white/20 rounded-full text-sm font-medium">
               {subcategory} 
             </span>
           </div>
