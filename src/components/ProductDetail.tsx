6cfc818c27296223220ac8061aa462c7e871a79c
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-18 13:05:07 +0300

    Updated ProductDetail.tsx

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1128,12 +1128,12 @@ const ProductDetail = () => {
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-6xl mx-auto">
            
-            <div className="rounded-2xl">
+           
               <div className="p-8">
                 <p className="text-gray-700 leading-relaxed whitespace-pre-line">
                   {displayedSafetyNote}
                 </p>
-              </div>
+             
             </div>
           </div>
         </div>
