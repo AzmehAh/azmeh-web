253bb20be834b600b8ccb795adf0e3c455ededdd
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-18 11:43:25 +0300

    Updated Blog.tsx

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -188,6 +188,7 @@ const Blog = () => {
         <div className="flex flex-col lg:flex-row gap-8">
           {/* Filters Sidebar */}
           <div className="lg:w-80 flex-shrink-0">
+             <div className="sticky top-24 max-h-[calc(100vh-6rem)] overflow-y-auto no-scrollbar">
             <div className="bg-white rounded-xl shadow-lg p-6 sticky top-24 max-h-[calc(100vh-6rem)] overflow-y-auto no-scrollbar">
               <div className="flex items-center justify-between mb-6">
                 <h3 className="text-xl font-bold text-gray-900">{t('blog.categoriesTitle')}</h3>
