ad6824c73428872806a6f950238301b94e20ac44
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-18 12:02:08 +0300

    Updated Blog.tsx

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -184,10 +184,10 @@ const Blog = () => {
       </div>
  
       {/* Content */}
-      <div className="max-w-7xl fixed mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-7xl  mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="flex flex-col lg:flex-row gap-8">
           {/* Filters Sidebar */}
-         <div className="lg:w-80 flex-shrink-0">
+         <div className="lg:w-80  fixed flex-shrink-0">
   <div className="  top-24">
     <div className="bg-white rounded-xl shadow-lg p-6 max-h-[calc(100vh-7rem)] overflow-y-auto no-scrollbar">
 
