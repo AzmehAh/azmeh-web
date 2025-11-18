35a881b09e23d5243d17dea254280e5ccc393140
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-18 13:31:36 +0300

    Updated ProductDetail.tsx

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -909,7 +909,7 @@ const ProductDetail = () => {
           <FileText className="w-5 h-5 mr-2 text-logo" />
           {t('products.technical_specifications')}
         </h2>
-        <div className="bg-white rounded-2xl shadow-sm border border-gray-200 overflow-hidden">
+        <div className="bg-white rounded-2xl shadow-sm border mt-1 mb-1 border-gray-200 overflow-hidden">
           <div className="divide-y divide-gray-100">
             {product.technical_specs.map((spec, index) => ( 
               <div key={index} className="px-6 py-4">
