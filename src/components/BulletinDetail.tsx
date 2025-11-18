3218517b1cdd4085644a57f706e49db886ebbc52
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-18 11:17:24 +0300

    Updated BulletinDetail.tsx

--- src/components/BulletinDetail.tsx
+++ src/components/BulletinDetail.tsx
@@ -198,7 +198,7 @@ const BulletinDetail = () => {
           <h1 className="text-2xl md:text-5xl font-bold mb-6 leading-tight">{title}</h1>
              <button
                   onClick={handleDownloadPDF}
-                  className="bg-black-400/15 hover:bg-white/20 text-white px-8 py-3 rounded-lg font-semibold transition-colors backdrop-blur-sm"
+                  className="bg-gray-300/15 hover:bg-white/20 text-white px-8 py-3 rounded-lg font-semibold transition-colors backdrop-blur-sm"
                 >
                   {t('bulletin.downloadPdf')}
                 </button>
