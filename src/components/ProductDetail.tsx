da95fc8ba78a34b44a2108929f5442b2d447dd16
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-20 13:41:31 +0300

    Updated ProductDetail.tsx

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -557,7 +557,7 @@ const handleDownloadPDF = async () => {
         row.style.breakInside = 'avoid';
         const lblCell = document.createElement('td');
         lblCell.style.fontWeight = 'bold';
-        lblCell.style.padding = '6px 8px';
+      
         lblCell.style.border = 'none';
         lblCell.style.width = '40%';
         lblCell.textContent = label;
