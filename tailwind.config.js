ded22b97dae338389c542d83f657d077d9e16ec7
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-25 11:15:17 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,37 @@ export default {
   theme: {
     extend: {
       colors: {
-        logo: "#003399",
+        logo: '#003399',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        // دعم المتصفحات الحديثة (مثل Firefox)
+        '.scrollbar-thin': {
+          'scrollbar-width': 'thin',
+          'scrollbar-color': '#003399 #f3f4f6',
+        },
+        // دعم WebKit (مثل Chrome, Safari)
+        '.scrollbar-thumb-logo': {
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: '#003399',
+            borderRadius: '8px',
+          },
+        },
+        '.scrollbar-track-gray-100': {
+          '&::-webkit-scrollbar-track': {
+            backgroundColor: '#f3f4f6',
+          },
+        },
+        '.scrollbar': {
+          '&::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px',
+          },
+        },
+      });
+    },
+  ],
 };
