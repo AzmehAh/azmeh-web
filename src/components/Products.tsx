0b7f20841e55ce80a387fad94eac66d402bd3221
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-25 11:18:29 +0300

    Updated Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -276,7 +276,10 @@ Object.entries(selectedFilters).forEach(([category, selectedValues]) => {
 
   
   return ( 
-    <div className="min-h-screen bg-gray-50 pt-20" dir={isRTL ? 'rtl' : 'ltr'}>
+ <div 
+  className="scrollable-page custom-scrollbar bg-gray-50 pt-20" 
+  dir={isRTL ? 'rtl' : 'ltr'}
+>
       <div className="text-logo pt-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-5xl md:text-6xl font-bold mb-6">
