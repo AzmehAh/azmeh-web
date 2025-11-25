a44fcd42e67a4955e750aa13651b23b3116062c7
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-11-25 15:05:37 +0300

    Updated Blog.tsx

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -175,7 +175,7 @@ const Blog = () => {
   );
 
   return (
-    <div className={`min-h-screen bg-gray-50 pt-28 ${isRTL ? 'rtl' : 'ltr'}`}>
+    <div className={`min-h-screen bg-gray-50 pt-28  scrollable-page custom-scrollbar ${isRTL ? 'rtl' : 'ltr'}`}>
       {/* Hero Section */}
       <div className="text-logo pt-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h1 className="text-5xl md:text-6xl font-bold mb-6">
